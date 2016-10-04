d0441441ccc006678cedcbc821e8fecf8586d63f
Julija <julija.kondrotaite@gmil.com>
2016-10-04 17:18:28 +0300

    pirmas programa

---
+++ pasaulis kompiuteryje/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pasaulis_kompiuteryje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("labas");
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
